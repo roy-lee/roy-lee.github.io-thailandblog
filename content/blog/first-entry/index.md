e3b7985165f4375ac1ca23c5704c12dbd17fcd54
Roy Lee <1700500@sit.singaporetech.edu.sg>
2019-04-18 01:10:57 +0800

    Rebase off Gatsby

---
+++ content/blog/first-entry/index.md
@@ -0,0 +1,15 @@
+---
+title: First Entry
+date: "2019-04-17"
+description: My 1st attempt at trying out a new way of blogging.
+---
+
+1st attempt at trying out ReactJS + GatsbyJS + Github Pages for blogging!
+
+1. [Gatsby Documentation](https://www.gatsbyjs.org/docs/)
+2. [Gatsby Starter Blog](https://github.com/gatsbyjs/gatsby-starter-blog)
+3. [Gatsby with Github Pages](https://www.gatsbyjs.org/docs/how-gatsby-works-with-github-pages/)
+
+Gatsby purely uses markdown for blog posts.
+
+CSS Styles are used too, of course. But that is a concern for another day.
---
+++ src/components/bio.js
@@ -0,0 +1,78 @@
+/**
+ * Bio component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
+
+import { rhythm } from "../utils/typography"
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, social } = data.site.siteMetadata
+        return (
+          <div
+            style={{
+              display: `flex`,
+              marginBottom: rhythm(2.5),
+            }}
+          >
+            <Image
+              fixed={data.avatar.childImageSharp.fixed}
+              alt={author}
+              style={{
+                marginRight: rhythm(1 / 2),
+                marginBottom: 0,
+                minWidth: 50,
+                borderRadius: `100%`,
+              }}
+              imgStyle={{
+                borderRadius: `50%`,
+              }}
+            />
+            <p
+              style={{
+                
+              }}>
+              Written by <strong>{author}</strong> who lives and studies in Singapore.
+              <br/>
+              {` `}
+              <a href={`https://github.com/${social.github}`}>
+                You can check out his Github
+              </a>
+            </p>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 100, height: 100) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+        social {
+          github
+        }
+      }
+    }
+  }
+`
+
+export default Bio
